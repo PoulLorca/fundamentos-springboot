e36c4c3b63a6fcd63b17357c5e428279ef6865c2
Poul Lorca <lorcapoul@gmail.com>
2023-03-31 17:19:40 -0300

    Ejemplo de creación de dependencia propia

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/component/ComponentToImplement.java
@@ -0,0 +1,11 @@
+package com.fundamentosplatzi.springboot.fundamentos.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentToImplement implements ComponentDependency{
+    @Override
+    public void saludar() {
+        System.out.println("Hola desde el componenete 2");
+    }
+}
